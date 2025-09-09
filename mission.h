d1f5d85101391824fc4d10a3ac93def9267cc5cf
ABA-ArThUr <arthurcasaurancqbzh@gmail.com>
2025-09-09 15:35:44 +0200

    ajout de mission.h

--- mission.h
+++ mission.h
@@ -1,10 +1,10 @@
 /*****************************************************************//**
- * \file   Entreprise.h
- * \brief  Declaration de la classe entreprise
- * 
- * \author Pierre
- * \date   June 2023
- *********************************************************************/
+* \file   mission.h
+* \brief  Declaration de la classe mission
+*
+* \author Arthur
+* \date   september 2025
+* ********************************************************************/
 
 #ifndef ENTREPRISE_H
 #define ENTREPRISE_H
@@ -12,46 +12,46 @@
 #include <string>
 
 
- /*! \class Entreprise
-    * \brief classe representant une entreprise. 2 attributs prives
-    * 
-    * Cette classe a une finalite pedagogique : 
-    * \li : Rappel du C++
-    * \li : commentaire avec Doxygen
-    * \li : git, github
-    */
-class Entreprise
+/*! \class mission
+   * \brief classe representant une mission. 3 attributs prives
+   */
+    class mission
 {
 
-    private:
-        /**
-         * Membre prive nomEntreprise : contient le nom de l'entreprise
-         */
-        std::string nomEntreprise; 
-        /**
-         * Membre prive villEntreprise : contient la ville de l'entreprise
-         */
-        std::string villeEntreprise; 
+private:
+    /**
+	 * Membre prive nbPersonne : contient le nombre de personnes pour la mission
+     */
+	std::string nbPersonne;
+    /**
+	 * Membre prive descriptifMission : contient le descriptif de la mission
+     */
+	std::string descriptifMission;
+    /**
+     * Membre prive nbHeure : contient le nombre d'heures de la mission
+	 */
+	std::string nbHeure;
 
 
 
-    protected : 
+protected:
 
 
 
-    public:
-        /*Classe sous forme canonique*/
-        Entreprise();   
-        ~Entreprise();  
-        Entreprise(const Entreprise& entre); 
-        Entreprise &operator = (const Entreprise& entre); 
-
-        Entreprise(std::string nom, std::string ville); 
-        std::string getNomEntreprise();
-        void setEntreprise(std::string nouveauNom);
-        std::string getVilleEntreprise();
-        void setVilleEntreprise(std::string nouvelleVille);
-       
+public:
+    /*Classe sous forme canonique*/
+	mission();
+	~mission(); 
+	mission(const mission& miss);
+	mission& operator = (const mission& miss);
+	mission(std::string personne, std::string mission, std::string heure);
+	std::string getNbPersonne();
+	void setNbPersonne(std::string nouveauNbPersonne);
+	std::string getDescriptifMission();
+	void setDescriptifMission(std::string nouveauDescriptifMission);
+	std::string getNbHeure();
+	void setNbHeure(std::string nouveauNbHeure);
+	};
 };
 
 #endif
