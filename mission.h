a9e02fd48e16c0af5c16b441487d96d007dc9845
ABA-ArThUr <arthurcasaurancqbzh@gmail.com>
2025-09-09 16:26:43 +0200

    erreur regler

--- mission.h
+++ mission.h
@@ -6,52 +6,46 @@
 * \date   september 2025
 * ********************************************************************/
 
-#define mission_H
 #ifndef MISSION_H
+#define MISSION_H
+
 #include <iostream>
 #include <string>
-
 
 /*! \class mission
    * \brief classe representant une mission. 3 attributs prives
    */
-    class mission
+class mission
 {
-
 private:
     /**
-	 * Membre prive nbPersonne : contient le nombre de personnes pour la mission
+     * Membre prive nbPersonne : contient le nombre de personnes pour la mission
      */
-	std::string nbPersonne;
+    std::string nbPersonne;
     /**
-	 * Membre prive descriptifMission : contient le descriptif de la mission
+     * Membre prive descriptifMission : contient le descriptif de la mission
      */
-	std::string descriptifMission;
+    std::string descriptifMission;
     /**
      * Membre prive nbHeure : contient le nombre d'heures de la mission
-	 */
-	std::string nbHeure;
-
-
+     */
+    std::string nbHeure;
 
 protected:
 
-
-
 public:
     /*Classe sous forme canonique*/
-	mission();
-	~mission(); 
-	mission(const mission& miss);
-	mission& operator = (const mission& miss);
-	mission(std::string personne, std::string mission, std::string heure);
-	std::string getNbPersonne();
-	void setNbPersonne(std::string nouveauNbPersonne);
-	std::string getDescriptifMission();
-	void setDescriptifMission(std::string nouveauDescriptifMission);
-	std::string getNbHeure();
-	void setNbHeure(std::string nouveauNbHeure);
-	};
+    mission();
+    ~mission();
+    mission(const mission& miss);
+    mission& operator = (const mission& miss);
+    mission(std::string personne, std::string mission, std::string heure);
+    std::string getNbPersonne();
+    void setNbPersonne(std::string nouveauNbPersonne);
+    std::string getDescriptifMission();
+    void setDescriptifMission(std::string nouveauDescriptifMission);
+    std::string getNbHeure();
+    void setNbHeure(std::string nouveauNbHeure);
 };
 
 #endif
