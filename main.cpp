d4306adaef7374d95379ed10a89eaf9f4587bdf1
ABA-ArThUr <arthurcasaurancqbzh@gmail.com>
2025-09-09 16:41:29 +0200

    erreur regler

---
+++ main.cpp
@@ -0,0 +1,13 @@
+// Your First C++ Program
+
+#include <iostream>
+#include "Entreprise.h"
+#include "mission.h"
+int main() {
+    std::cout << "Hello World!";
+    Entreprise entre1;
+    Entreprise maSuperEntre;
+	mission mamission;
+    
+    return 0;
+}
