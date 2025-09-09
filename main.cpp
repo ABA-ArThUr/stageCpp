c33ac3976b712fab33b5b9629d1e79efa59eed3f
ABA-ArThUr <arthurcasaurancqbzh@gmail.com>
2025-09-09 10:20:44 +0200

    debut de la creation d'objet pour les missions

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/*****************************************************************//**
+ * \file   Entreprise.cpp
+ * \brief  Definition de la classe Entreprise
+ *
+ * \author arthur
+ * \version 0.1
+ * \date   september 2025
+ *
+ * Programme Cpp permettant de creer des objets pour décrire les missions
+ *********************************************************************/
+
+#include "entreprise.h"
+
+
+
+using namespace std;
+
